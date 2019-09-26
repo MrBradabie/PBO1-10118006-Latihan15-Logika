a90b71db7aeda08d7ccc31edbdf8f4574b490e9c
MrBradabie <55830472+MrBradabie@users.noreply.github.com>
2019-09-26 16:55:29 +0700

    Add files via upload

---
+++ PBO1-10118006-Latihan15-Logika/src/pbo1/pkg10118006/latihan15/logika/PBO110118006Latihan15Logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118006.latihan15.logika;
+
+/**
+ *
+ * @author 
+ * NAMA  : Alfarizi Rizki Pane
+ * KELAS : PBO1
+ * NIM   : 10118006
+ * Deskripsi Program : Program ini berisi  program untuk Logika
+ */
+public class PBO110118006Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + ! (a && b));
+    }
+    
+}
